8659e8988b52d5dafe0cb364278c4863a7c26adb
robertadelima <robertarl@gmail.com>
2019-11-09 14:04:22 -0300

    Created entities

---
+++ backend/Domain/Blog.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace backend.Domain
+{
+    public class Blog : Entity
+    {
+       public string description;
+
+
+    }
+}
---
+++ backend/Domain/Entity.cs
@@ -0,0 +1,11 @@
+using System;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace backend.Domain
+{
+    public class Entity 
+    {
+        public int id;  
+    }
+}
