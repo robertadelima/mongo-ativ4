f9b60008ac6f38fd654b7d1180ab6a414b191288
robertadelima <robertarl@gmail.com>
2019-11-09 11:06:59 -0300

    converted to console and connected to mongo

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace Ativ4Mongo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            new Program().testConnection();
+        }
+
+        public void testConnection(){
+             var client = new MongoClient("mongodb://localhost:27017");
+             var database = client.GetDatabase("foo");
+             var collection = database.GetCollection<BsonDocument>("bar");
+
+            collection.InsertOne(new BsonDocument("Name", "Jack"));
+
+            var list = collection.Find(new BsonDocument("Name", "Jack")).ToList();
+
+            foreach(var document in list)   
+            {
+                Console.WriteLine(document["Name"]);    
+            }   
+        }
+    }
+}
