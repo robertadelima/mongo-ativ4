4c7d5fd9f53c5fd3b289a771bb264e5fc1b68191
mabo3n <dolly.marcel@gmail.com>
2019-11-18 22:54:51 -0300

    Fix PostsController routes

--- backend/Api/Controllers/PostsController.cs
+++ backend/Api/Controllers/PostsController.cs
@@ -13,7 +13,7 @@ using Microsoft.Extensions.Logging;
 namespace Ativ4Mongo.backend.Api.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("Blogs/{username}")]
     public class PostsController : ControllerBase
     {
         private readonly PostRepository postRepository;
@@ -21,11 +21,12 @@ namespace Ativ4Mongo.backend.Api.Controllers
         public PostsController(PostRepository postRepository)
         {
             this.postRepository = postRepository;
-        } 
+        }
 
+        /* // Isto esta dando conflito com o endpoint de BlogsController.GetByUserName
         [HttpGet]
-        [Route("{username}/[controller]")]
-        public IEnumerable<PostPreviewViewModel> Get(string username)
+        [Route("")]
+        public IEnumerable<PostPreviewViewModel> GeyByUsername(string username)
         {
             //TODO 
             //REFACTOR
@@ -35,11 +36,12 @@ namespace Ativ4Mongo.backend.Api.Controllers
                     FirstContent = entidade.firstContent,
                 } );
         }
+        */
 
         [HttpPost]
         [Route("")]
-        public IActionResult CreatePost([FromBody] PostPreviewViewModel postViewModel){
-            
+        public IActionResult CreatePost([FromBody] PostPreviewViewModel postViewModel)
+        {
             if (postViewModel == null)
             {
                 return BadRequest();
@@ -57,17 +59,17 @@ namespace Ativ4Mongo.backend.Api.Controllers
 
         [HttpDelete]
         [Route("{title}")]
-        public IActionResult Delete(string title){
+        public IActionResult Delete(string title)
+        {
             var post = postRepository.getPostsByTitle(title);
-
-            if(post == null)
+            if (post == null)
+            {
                 return NotFound();
+            }
             
             postRepository.Remove(title);
+
             return new NoContentResult();
         }
-
-
-
     }
 }
