e4fcbff663e5ed41ef31562d02a40512caf19ad2
robertadelima <robertarl@gmail.com>
2019-11-22 21:00:07 -0300

    frontend dependecies

--- Startup.cs
+++ Startup.cs
@@ -21,11 +21,21 @@ namespace Ativ4Mongo
             Configuration = configuration;
         }
 
+        readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+            options.AddPolicy(MyAllowSpecificOrigins,
+            builder =>
+            {
+                builder.WithOrigins("http://localhost:8080");
+            });
+        });
             services.AddControllers();
             services.AddMvc().AddJsonOptions(options => {
                 options.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
@@ -48,6 +58,8 @@ namespace Ativ4Mongo
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors(MyAllowSpecificOrigins);
 
             app.UseEndpoints(endpoints =>
             {
--- backend/Api/Controllers/BlogsController.cs
+++ backend/Api/Controllers/BlogsController.cs
@@ -5,6 +5,7 @@ using Ativ4Mongo.backend.Api.Payloads;
 using Ativ4Mongo.backend.Domain;
 using Ativ4Mongo.backend.Infra.Repository;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Cors;
 
 namespace Ativ4Mongo.backend.Api.Controllers
 {
@@ -19,6 +20,7 @@ namespace Ativ4Mongo.backend.Api.Controllers
             this.blogRepository = blogRepository;
         }
 
+        [EnableCors]
         [HttpGet]
         [Route("")]
         public IEnumerable<BlogPreviewViewModel> GetBlogs()
